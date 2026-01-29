10da544380451619bac4cc8ce1d2462c9d7802e3
Witalij <witalij.rapicki@gmail.com>
2026-01-29 13:10:53 +0100

    feat: fix spa routing for cloudfrnt

--- infra/frontend/cloudfront_function.js
+++ infra/frontend/cloudfront_function.js
@@ -2,16 +2,9 @@ function handler(event) {
   var request = event.request;
   var uri = request.uri;
 
-  // jeśli katalog (np. /contact/)
-  if (uri.endsWith('/')) {
-    request.uri = uri + 'index.html';
-    return request;
-  }
-
-  // jeśli brak rozszerzenia (np. /contact)
-  if (!uri.includes('.')) {
-    request.uri = uri + '/index.html';
-    return request;
+  // wszystko co nie jest plikiem → index.html
+  if (!uri.includes(".")) {
+    request.uri = "/index.html";
   }
 
   return request;
