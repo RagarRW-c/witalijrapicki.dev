08148155bff89ede643da3c85a62a71ed74bf433
Witalij <witalij.rapicki@gmail.com>
2026-01-29 13:44:46 +0100

    feat: fix correct Astro routing in CLoudFront Function

--- infra/frontend/cloudfront_function.js
+++ infra/frontend/cloudfront_function.js
@@ -2,15 +2,13 @@ function handler(event) {
   var request = event.request;
   var uri = request.uri;
 
-  // jeśli katalog (np. /contact/)
-  if (uri.endsWith('/')) {
-    request.uri = uri + 'index.html';
+  if (uri.endsWith("/")) {
+    request.uri = uri + "index.html";
     return request;
   }
 
-  // jeśli brak rozszerzenia (np. /contact)
-  if (!uri.includes('.')) {
-    request.uri = uri + '/index.html';
+  if (!uri.includes(".")) {
+    request.uri = uri + "/index.html";
     return request;
   }
 
