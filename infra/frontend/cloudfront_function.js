c159c15582fd9d1bc47eab39eb00c6059b51e943
Witalij <witalij.rapicki@gmail.com>
2026-01-29 13:25:39 +0100

    feat: uodate cloudfront function

--- infra/frontend/cloudfront_function.js
+++ infra/frontend/cloudfront_function.js
@@ -2,9 +2,16 @@ function handler(event) {
   var request = event.request;
   var uri = request.uri;
 
-  // wszystko co nie jest plikiem → index.html
-  if (!uri.includes(".")) {
-    request.uri = "/index.html";
+  // jeśli katalog (np. /contact/)
+  if (uri.endsWith('/')) {
+    request.uri = uri + 'index.html';
+    return request;
+  }
+
+  // jeśli brak rozszerzenia (np. /contact)
+  if (!uri.includes('.')) {
+    request.uri = uri + '/index.html';
+    return request;
   }
 
   return request;
