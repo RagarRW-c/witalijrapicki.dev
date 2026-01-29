2ad803e213c7f5de6f399205e584c5b9c9dbfd59
Witalij <witalij.rapicki@gmail.com>
2026-01-29 17:02:08 +0100

    feat: change in handler

--- infra/contact/lambda.tf
+++ infra/contact/lambda.tf
@@ -1,8 +1,9 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda"
+  source_file = "${path.module}/lambda/handler.py"
   output_path = "${path.module}/lambda.zip"
 }
+
 
 resource "aws_lambda_function" "contact" {
   function_name = "contact-form"
