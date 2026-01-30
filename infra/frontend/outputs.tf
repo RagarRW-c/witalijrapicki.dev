3dbfef31853d26bb9d02511ed2e237aa85893150
Witalij <witalij.rapicki@gmail.com>
2026-01-30 11:52:06 +0100

    feat: change in output

--- infra/frontend/outputs.tf
+++ infra/frontend/outputs.tf
@@ -2,10 +2,6 @@ output "cloudfront_domain" {
   value = aws_cloudfront_distribution.prod.domain_name
 }
 
-output "contact_api_domain" {
-  value = replace(
-    aws_api_gateway_stage.prod.invoke_url,
-    "https://",
-    ""
-  )
+output "contact_api_url" {
+  value = aws_api_gateway_stage.prod.invoke_url
 }
